f028172cc5f8f69b717b57ef0c3d819955f0f8af
Disha Acharya <dishada3004@gmail.com>
2025-06-03 13:14:40 +0530

    first commit

--- client1.py
+++ client1.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-client = Client('https://cloud-00y8.onrender.com')
+client = Client('https://cloud-00y8.onrender.com/?wsdl')
 
 HTML = '''
     <form method="post">
